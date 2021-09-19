4c95287a548af05d1d4a09fb3f2dab237e20b239
sushant2931 <sushantlad8888@gmail.com>
2021-09-19 23:49:32 +0530

    Added: Validating FirstName using J unit test

--- src/test/java/com/bridgelabz/userregistrationjunittest/UserRegistrationTest.java
+++ src/test/java/com/bridgelabz/userregistrationjunittest/UserRegistrationTest.java
@@ -1,4 +1,25 @@
 package com.bridgelabz.userregistrationjunittest;
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
 public class UserRegistrationTest {
+    UserRegistration userRegistration;
+    @Before
+    public void before() {
+        userRegistration = new UserRegistration();
+    }
+
+    @Test
+    public void givenFirstName_WhenProper_ShouldReturnTrue() {
+        boolean result = userRegistration.firstName("Prash");
+        Assert.assertEquals(true, result);
+    }
+
+    @Test
+    public void givenFirstName_WhenNotProper_ShouldReturnFlase() {
+        boolean result = userRegistration.firstName("prash");
+        Assert.assertEquals(false, result);
+    }
 }
