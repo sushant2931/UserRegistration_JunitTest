c7f8aea261555512cbe1610dcb1c7257f83cdf2e
sushant2931 <sushantlad8888@gmail.com>
2021-09-19 23:41:23 +0530

    Added: Welcome On User Registration Program Using Junit Test

---
+++ src/main/java/com/bridgelabz/userregistrationjunittest/UserRegistration.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.userregistrationjunittest;
+
+public class UserRegistration {
+    public static void main(String[] args) {
+        System.out.println("....... Welcome On User Registration Program Using Junit Test......");
+    }
+}
---
+++ src/test/java/com/bridgelabz/userregistrationjunittest/UserRegistrationTest.java
@@ -0,0 +1,4 @@
+package com.bridgelabz.userregistrationjunittest;
+
+public class UserRegistrationTest {
+}
