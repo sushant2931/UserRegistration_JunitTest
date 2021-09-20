1e0b0efc5369e2d5be3f437fce02e82b6a1d3267
sushant2931 <sushantlad8888@gmail.com>
2021-09-20 08:22:07 +0530

    Added :Validating all Email Using Junit Paremetrized test

--- src/main/java/com/bridgelabz/userregistrationjunittest/UserRegistration.java
+++ src/main/java/com/bridgelabz/userregistrationjunittest/UserRegistration.java
@@ -19,7 +19,7 @@ public class UserRegistration {
     }
 
     public boolean email(String email) {
-        String regex = "^[a-z0-9]{3,}+([_+-.][a-z0-9]{3,}+)*@[a-z0-9]+.[a-z]{2,3}+(.[a-z]{2,3}){0,1}$";
+        String regex = "^[a-z]{3,}([_+-.]?[a-z0-9]{3,}+)*@[a-z0-9]+.[a-z]{2,3}+([.,][a-z]{2,3}+)*$";
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(email);
         return matcher.matches();
@@ -63,10 +63,9 @@ public class UserRegistration {
     }
 
     public boolean emailIdValidator(String emailId) {
-        String regex = "^[a-z0-9]{3,}+([_+-.][a-z0-9]{3,}+)*@[a-z0-9]+.[a-z]{2,3}+(.[a-z]{2,3}){0,1}$";
+        String regex = "^[a-z]{3,}+([_+-.][a-z0-9]{3,}+)*@[a-z0-9]+.[a-z]{2,3}+([.,]?[a-z]{2,3}){0,1}$";
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(emailId);
         return matcher.matches();
     }
-
 }
